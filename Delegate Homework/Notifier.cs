4e9c53bc1069b918347077f8db291bb2448b5d3d
ElikkDev <eljanfn-bpa202@code.edu.az>
2025-11-07 01:20:41 +0400

    First task

---
+++ Delegate Homework/Notifier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Homework
+{
+    public delegate void NotificationHandler(string message);
+    public class Notifier
+    {
+        public NotificationHandler? NotificationEvent;
+
+        public void SendNotification(string message)
+        {
+            if (NotificationEvent != null)
+                NotificationEvent(message);
+            else
+                Console.WriteLine("no subscribers");
+        }
+    }
+}
---
+++ Delegate Homework/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Delegate_Homework;
+
+class Program
+{
+    static void Main()
+    {
+        Notifier notifier = new Notifier();
+
+        EmailService emailService = new EmailService();
+        PushNotificationService pushNotification = new PushNotificationService();
+        SMSService sMSService = new SMSService();
+
+        notifier.NotificationEvent += emailService.SendEmail;
+        notifier.NotificationEvent += pushNotification.SendPush;
+        notifier.NotificationEvent += sMSService.SendSMS;
+
+        Console.WriteLine("Enter message:");
+        string msg = Console.ReadLine();
+
+        Console.WriteLine("First deliver:");
+        notifier.SendNotification(msg);
+
+
+        notifier.NotificationEvent -= sMSService.SendSMS;
+
+        Console.WriteLine("Message after delivering");
+        notifier.SendNotification(msg);
+
+
+
+    
+
+    }
+}
